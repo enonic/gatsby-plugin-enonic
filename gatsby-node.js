38e10447012cea4ac5463f1e6e7bf2a8e4f692c5
Alan Semenov <ase@enonic.com>
2020-03-10 17:19:58 +0100

    Changed to a different way of query file resolution. Changed format of application placeholder. Added error validation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,22 @@
 const _ = require(`lodash`);
-const appRoot = require('app-root-path');
 const gatsbySourceGraphQLNode = require(`gatsby-source-graphql/gatsby-node`);
 
 const schemaName = 'XP';
 const schemaPrefix = `${schemaName}_`;
 
 exports.sourceNodes = async (
-  utils,
-  {
-      typeName = schemaName,
-      fieldName = schemaName.toLowerCase(),
-      ...options
-  }
+    utils,
+    {
+        typeName = schemaName,
+        fieldName = schemaName.toLowerCase(),
+        ...options
+    }
 ) => {
     const { url, refetchInterval } = options;
+    const { reporter } = utils;
 
     if (!url) {
-        throw new Error(`gatsby-plugin-enonic requires GraphQL API endpoint to be specified (\`options.url\`)`);
+        reporter.panic(`gatsby-plugin-enonic requires GraphQL API endpoint to be specified (\`options.url\`)`);
     }
 
     await gatsbySourceGraphQLNode.sourceNodes(utils, {
@@ -31,17 +31,17 @@ exports.sourceNodes = async (
 exports.createPages = async ({ graphql, actions, reporter }, options) => {
     const { application, pages } = options;
 
-    if (!pages) {
-        throw new Error(`gatsby-plugin-enonic requires at least one page definition to be specified (\`options.pages\`)`);
+    if (!pages || !pages.length) {
+        reporter.panic(`gatsby-plugin-enonic requires at least one page definition to be specified (\`options.pages\`)`);
     }
 
     const { createPage } = actions;
     const schemaTypes = await getContentTypes(graphql, reporter);
 
     return await Promise.all(
-      pages.map(async pageDef =>
-        await createCustomPages(graphql, createPage, reporter, pageDef, schemaTypes, application)
-      )
+        pages.map(async pageDef =>
+            await createCustomPages(graphql, createPage, reporter, pageDef, schemaTypes, application)
+        )
     )
 };
 
@@ -55,7 +55,7 @@ const processTypesInQuery = async (query, types) => {
 
 const getContentTypes = async (graphql, reporter) => {
     const result = await graphql(
-    `{
+        `{
       __schema {
         types {
           name
@@ -74,7 +74,7 @@ const getContentTypes = async (graphql, reporter) => {
     const contentInterface = result.data.__schema.types.filter(type => type.name === `${schemaPrefix}Content`)[0];
 
     if (!contentInterface || !contentInterface.possibleTypes || !contentInterface.possibleTypes.length) {
-        throw new Error(`No content types found in GraphQL schema!`);
+        reporter.panic('gatsby-plugin-enonic failed to find content types in the GraphQL schema!');
     }
 
     return contentInterface.possibleTypes.map(type => type.name.slice(schemaPrefix.length));
@@ -83,19 +83,27 @@ const getContentTypes = async (graphql, reporter) => {
 const sanitizeTemplate = (queryTemplate, application) => {
     const _app = `${application}_`.replace(/\./g, '_');
 
-    let result = queryTemplate.replace(/{application}_/, _app); // Replace "{application}_" with "com_example_myproject_"
-    result = result.replace(/{application}/, application);      // Replace "{application}" with "com.example.myproject"
+    let result = queryTemplate.replace(/%application%_/, _app); // Replace "%application%_" with "com_example_myproject_"
+    result = result.replace(/%application%/, application);      // Replace "%application%" with "com.example.myproject"
 
     return result;
 };
 
 const createCustomPages = async (graphql, createPage, reporter, pageDef, schemaTypes, application) => {
-    const queryTemplate = require(appRoot + pageDef.query);
+    if (!pageDef.query) {
+        reporter.panic('gatsby-plugin-enonic requires query in page definition (`options.pages.query`)')
+    }
+    const queryTemplate = require(pageDef.query);
+
+    if (typeof queryTemplate !== 'string' || !queryTemplate.trim()) {
+        reporter.panic(`gatsby-plugin-enonic failed to find query at ${pageDef.query}`);
+    }
+
     const sanitizedTemplate = application ? sanitizeTemplate(queryTemplate, application) : queryTemplate;
     const query = await processTypesInQuery(sanitizedTemplate, schemaTypes);
     const result = await graphql(
     `{
-      xp {
+      ${schemaName.toLowerCase()} {
         guillotine {
             nodes: ${query}
         }
@@ -109,32 +117,49 @@ const createCustomPages = async (graphql, createPage, reporter, pageDef, schemaT
 
     const nodes = result.data.xp.guillotine.nodes;
 
-    const detailsPagePath = (pageDef.details ? pageDef.details.path : '') || pageDef.list.path;
+    const detailsPageUrl = (pageDef.details ? pageDef.details.url : '') || (pageDef.list ? pageDef.list.url : '');
 
     if (pageDef.list) {
 
+        if (!pageDef.list.url) {
+            reporter.panic('gatsby-plugin-enonic requires url for the list page (`options.pages.list.url`)');
+        }
+
+        if (!pageDef.list.template) {
+            reporter.panic('gatsby-plugin-enonic requires template for the list page (`options.pages.list.template`)');
+        }
+
         // Create node list pages
         createPage({
-            path: pageDef.list.path,
-            component: require.resolve(pageDef.list.template),
+            path: pageDef.list.url,
+            component: pageDef.list.template,
             context: {
                 nodes,
-                detailsPath: detailsPagePath
+                detailsPageUrl: detailsPageUrl,
+                detailsPageKey: pageDef.details ? pageDef.details.key : null
             },
         })
     }
 
     if (pageDef.details) {
 
+        if (!pageDef.details.key) {
+            reporter.panic('gatsby-plugin-enonic requires id field for the details page (`options.pages.details.key`)');
+        }
+
+        if (!pageDef.details.template) {
+            reporter.panic('gatsby-plugin-enonic requires template for the list page (`options.pages.details.template`)');
+        }
+
         // Create individual node pages
         nodes.forEach(node => {
 
             createPage({
-                path: `${detailsPagePath}/${_.kebabCase(node[pageDef.details.key])}`,
-                component: require.resolve(pageDef.details.template),
+                path: `${detailsPageUrl}/${_.kebabCase(node[pageDef.details.key])}`,
+                component: pageDef.details.template,
                 context: {
                     node,
-                    listPath: pageDef.list.path
+                    listPageUrl: pageDef.list ? pageDef.list.url : ''
                 },
             })
         })
